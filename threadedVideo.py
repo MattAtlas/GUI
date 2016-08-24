413bcd0d9a546955d363c42622519f32068a3a6c
MattAtlas <saltattam@gmail.com>
2016-08-23 20:04:46 -0700

    Update threadedVideo.py

--- threadedVideo.py
+++ threadedVideo.py
@@ -9,76 +9,114 @@ import Tkinter as tk
 from PIL import ImageTk, Image
 import threading
 from multiprocessing import Process, Lock, Manager #multiprocessing
+import Queue
+
+
+class getFrame(threading.Thread):
+	def __init__(self, master, q):
+		threading.Thread.__init__(self)
+		self.master = master
+		self.q = q
+		HOST = 'localhost'
+		PORT = 8888
+		self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+		print "Socket created"
+
+		ADDR = (HOST,PORT)
+		self.sock.bind(ADDR)
+		self.run()
+
+	def recvAll(self):
+
+		buff = 4096*2
+		data = ""
+		while True:
+			raw_data = self.sock.recv(12+buff)
+			F, N, I, i = struct.unpack('>IIHH', raw_data[0:12]) 		# actually [0:9]
+			if i == 0:													# first packet of frame
+				break
+		data += raw_data[12:buff+12]									# actually [10:buff+10]
+		frame = F
+		n = 1
+		while True:
+			raw_data = self.sock.recv(12+buff)
+			F, N, I, i = struct.unpack('>IIHH', raw_data[0:12]) 		# actually [0:9]
+			if F != frame:
+				data = None
+				break
+			data += raw_data[12:buff+12]								# actually [10:buff+10]
+			n += 1
+			if n > I:
+				break
+		return data
+
+
+	def run(self):
+		if self.q.empty() == True:
+			data = self.recvAll()
+			self.q.put(data)
+			sys.stdout.write("Frames in queue : %s\r" % str(self.q.qsize()))
+			sys.stdout.flush()
+		self.master.update_idletasks()
+		self.master.after(0, lambda: self.run())
+
 
 
 class Video(threading.Thread):
-	def __init__(self, master):
+	def __init__(self, master, q):
 		threading.Thread.__init__(self)
 
 		self.master = master
-		HOST = 'localhost'
-		PORT = 8888
-		self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		print "Socket created"
-
-		server_address = (HOST,PORT)
-		self.sock.connect(server_address)
-
+		self.q = q
 		self.vidFrame = tk.Frame(master, width=640, height=480)
 		self.vidFrame.pack(fill=tk.BOTH)
-
 		self.vidLabel = tk.Label(self.vidFrame)
 		self.vidLabel.pack(fill=tk.BOTH)
-		
+
 		self.showVideo()
 
-	def recv_msg(self, buff):
-		i = 0
-		size = ""
-		for i in range(0,4):
-		#while i < 4:
-			raw_size = self.sock.recv(1)
-			size += "%s" % (raw_size.encode('hex'))
-		#	i += 1
-		size = int(size,16)
-		#print "4 byte size packet = " + str(size)
-		data = ""
-		while size > 0:
-			if size > buff:
-				data += self.sock.recv(buff)
-				size -= buff
-			else:
-				data += self.sock.recv(size)
-				size = 0
-		return data
 
 	def showVideo(self):
 		
-		raw_data = self.recv_msg(1024)		# string
-		b_data = np.fromstring(raw_data, dtype="uint8")
-		decimg = cv2.imdecode(b_data,1)
-		decimg = np.reshape(decimg,(480,640,3))
+		if self.q.empty() == False:
+			raw_data = self.q.get()
+			self.q.task_done()
 
-		img = Image.fromarray(decimg)
-		imgtk = ImageTk.PhotoImage(image=img)
-		self.vidLabel.configure(image=imgtk)
-		self.vidLabel.image = imgtk
+			if raw_data != None:
+				b_data = np.fromstring(raw_data, dtype="uint8")
+				decimg = cv2.imdecode(b_data,1)
+				decimg = np.reshape(decimg,(480,640,3))
+				b,g,r = cv2.split(decimg)
+				decimg = cv2.merge((r,g,b))
+
+				img = Image.fromarray(decimg)
+				imgtk = ImageTk.PhotoImage(image=img)
+				self.vidLabel.configure(image=imgtk)
+				self.vidLabel.image = imgtk
+
 		self.master.update_idletasks()
-		self.master.after(0, self.showVideo)
+		self.master.after(0, lambda: self.showVideo())
+
+
+
 
 class tkinterGUI(tk.Frame):
 	def __init__(self):
 		root = tk.Frame.__init__(self)
-		top=self.winfo_toplevel()
+		top = self.winfo_toplevel()
 		top.wm_geometry("640x480")
 		self.pack()
 
-		videoThread  = Video(self)
+		q = Queue.Queue(maxsize=0)
+		
+		getFrameThread = getFrame(self, q)
+		videoThread  = Video(self, q)
 
+		getFrameThread.setDaemon(True)
 		videoThread.setDaemon(True)
-
+		
 		videoThread.start()
-
+		getFrameThread.start()
 		print '# active threads are ',threading.enumerate()
 
 
