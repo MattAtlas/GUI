a0ef738aaa59e8aab2a556ae1f48da7690abf812
MattAtlas <saltattam@gmail.com>
2016-08-23 20:05:24 -0700

    Update server.py

--- server.py
+++ server.py
@@ -5,67 +5,73 @@ import numpy as np
 import sys
 import struct
 import errno
+import threading
+from Queue import Queue
+#from matplotlib import pyplot as plt
 
-# Function to back the length of message with message and send to socket
-def send_msg(conn, msg):
-	msg = struct.pack('>I',len(msg)) + msg
-	#print len(msg)
-	conn.sendall(msg)
+def send_data(out_q, sock, addr):
+	frame = 0
+	while True:
+		data = out_q.get()
+		frame += 1
+	
+		buff = 4096*2
+		
+		N = len(data) 		# length of actual data in bytes
+		n = N
+		I = N/buff 			# number of required dgrams including 0
+		i = 0
+		for i in range(0,I+1):
+			if n > buff:
+				dgram = data[i*buff:((i+1)*buff)] 	
+				header = struct.pack('>IIHH', frame, N, I, i)   # 10 bytes
+				dgram = header + dgram
+				sock.sendto(dgram, addr)
+				n -= buff
+			else:
+				dgram = data[i*buff:N]
+				header = struct.pack('>IIHH', frame, N, I, i)
+				dgram = header + dgram
+				sock.sendto(dgram, addr)
+	
+		
+		#time.sleep(0.5)
 
 
-# Get TCP socket ready for client.  Code continues after connection
-HOST = 'localhost'
-PORT = 8888
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print "Socket created"
-server_address = (HOST,PORT)
-try:
-	sock.bind(server_address)
-	print "Socket bind complete"
-except socket.error as msg:
-	print "Bind failed. Error code : " + str(msg[0]) + " Message " + msg[1]
-	sys.exit()
-sock.listen(1)
-print "Waiting for client to connect"
-conn, addr = sock.accept()
-print 'Connected with ' + addr[0] + ':' + str(addr[1])
 
-# OpenCV camera object
-vc = cv2.VideoCapture(1)
+def read_cam(in_q):
+	start = time.time()
+	time.sleep(0.01)
+	while True:
 
-while True:
-	try:
+		start = time.time()
 		retval,image = vc.read()
-		# Rearrang the color channels
-		b,g,r = cv2.split(image)
-		image = cv2.merge((r,g,b))
+		deltaT = time.time() - start
+		#print "time to read camera: "+str(deltaT)+"\r"
+
 		# Make into numpy array
 		image = np.array(image,dtype="uint8")
-		# Encode into jpeg 
-		encode_param = [cv2.IMWRITE_JPEG_QUALITY,80]
+		encode_param = [cv2.IMWRITE_JPEG_QUALITY,50]
 		result,encimg = cv2.imencode(".jpg",image,encode_param)
-		# Flatten and make into string ready for transmission
 		packet = encimg.flatten().tostring()
-		# Send packet to socket
-		send_msg(conn, packet)
-		# Wait time for next frame
+		in_q.put(packet)
 		time.sleep(0.04)
 
-	except socket.error, e:
-		if isinstance(e.args, tuple):
-			print "errno is %d" % e[0]
-			if e[0] == errno.EPIPE:
-			   # remote peer disconnected
-			   print "Detected remote disconnect"
-			else:
-			   # determine and handle different error
-			   pass
-		else:
-			print "socket error ", e
-		conn.close()
-		break
-	except IOError, e:
-		# Hmmm, Can IOError actually be raised by the socket module?
-		print "Got IOError: ", e
-		break
+
+q = Queue()
+
+
+HOST = 'localhost'
+PORT = 8888
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+addr = (HOST,PORT)
+
+vc = cv2.VideoCapture(0)
+
+t1 = threading.Thread(target=read_cam, args=(q,))
+t2 = threading.Thread(target=send_data, args=(q,sock,addr))
+
+t2.start()
+t1.start()
+
 
